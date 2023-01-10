c00acf624fe8c1c75e63a4892826dcf867704e30
Sergey Sapronov <1262708@mail.ru>
2023-01-10 18:40:56 +0300

    Update db.php
    default pswd

--- htdocs/config/db.php
+++ htdocs/config/db.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=zaotoni_vestnik',
-    'username' => 'zaotoni_vestnik',
-    'password' => 'hwDA2qeJ',
+    'dsn' => 'mysql:host=localhost;dbname=name_your_db',
+    'username' => 'username',
+    'password' => 'password',
     'charset' => 'utf8',
     'tablePrefix' => 'vest_'
 ];
